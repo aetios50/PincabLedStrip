a80b2edb34db0840e8d7d8eda3699566934cde2c
aetios50 <frenchclanclan@gmail.com>
2017-06-27 19:29:17 +0200

    Update LedStrip.h

--- LedStrip.h
+++ LedStrip.h
@@ -1,52 +1,58 @@
 #ifndef LedStrip_h
 #define LedStrip_h
 
-/***/
-//#define DEBUG_ON_WIFI
-//#ifdef DEBUG_ON_WIFI
-//#include <WifiDebug.h>
-//static WifiDebug wifidebug;
-//#endif
-/***/
-
 #include <Arduino.h>
-/*
-#define FASTLED_ESP8266_D1_PIN_ORDER
+
+#ifndef DEBUG_ON_WIFI
 #define FASTLED_ALLOW_INTERRUPTS 0
-#define FASTLED_INTERRUPT_RETRY_COUNT 0
-*/
+#endif
+
 #include <FastLED.h>
 FASTLED_USING_NAMESPACE
 
+
+#define DATA_PIN1   12 // GPIO12 / D6 on WeMos/NodeMCU ESP8266
+#define DATA_PIN2   13 // D7 
+#define DATA_PIN3   14 // D8
+
+#define LED_TYPE    WS2811
+#define COLOR_ORDER RGB
+//#define COLOR_ORDER GRB
+
+/*************   VALUE TO CHANGE  *******************/
+//Defines the max number of ledstrip which is allowed per ledstriptrip.
+#define MaxLedsPerStrip 432
+//Defines the number of ledstrip
+#define NUMBER_LEDSTRIP 1
+//Defines Brightness
+#define BRIGHTNESS  255
+/*************   END VALUE TO CHANGE  *******************/
+
 class LedStrip {
-public:
-LedStrip(uint32_t numPerStrip,CRGB *leds);
-void begin(void);
+  public:
+    LedStrip(uint32_t numPerStrip);
+    void begin(void);
 
-void setStripLength(uint16_t length);
-void setPixel(uint32_t num, int color);
-void setPixel(uint32_t num, uint8_t red, uint8_t green, uint8_t blue) {
-								setPixel(num, color(red, green, blue));
-}
-int getPixel(uint32_t num);
+    void setStripLength(uint16_t length);
+    void setPixel(uint32_t num, int color);
+    void setPixel(uint32_t num, uint8_t red, uint8_t green, uint8_t blue) {
+      setPixel(num, color(red, green, blue));
+    }
+    int getPixel(uint32_t num);
 
-void show(void);
-int busy(void);
+    void show(void);
+    int busy(void);
 
-int numPixels(void) {
-								return stripLen * 8;
-}
-int color(uint8_t red, uint8_t green, uint8_t blue) {
-								return (red << 16) | (green << 8) | blue;
-}
+    int numPixels(void) {
+      return stripLen * 8;
+    }
+    int color(uint8_t red, uint8_t green, uint8_t blue) {
+      return (red << 16) | (green << 8) | blue;
+    }
 
 
-private:
-static uint16_t stripLen;
-static void *frameBuffer;
-static void *drawBuffer;
-static uint8_t params;
-static void isr(void);
+  private:
+    static uint16_t stripLen;
 };
 
 #endif
