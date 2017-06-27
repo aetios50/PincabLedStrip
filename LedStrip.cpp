ddd0177189fd4b44e134d62372a9c0aeb5713300
aetios50 <frenchclanclan@gmail.com>
2017-06-27 19:28:21 +0200

    Update LedStrip.cpp

--- LedStrip.cpp
+++ LedStrip.cpp
@@ -1,77 +1,60 @@
 #include <string.h>
 #include "LedStrip.h"
 
+
 uint16_t LedStrip::stripLen;
 
-#define DATA_PIN1   0 // Pin du ledstrip gauche
-#define DATA_PIN2   2 // Pin du ledstrip droit
-#define BRIGHTNESS  200
-#define LED_TYPE    WS2812B
-//#define COLOR_ORDER RGB
-#define COLOR_ORDER GRB
-static CRGB *leds;
+CRGB leds[NUMBER_LEDSTRIP][MaxLedsPerStrip];
 
 
-LedStrip::LedStrip(uint32_t numPerStrip,CRGB * ledsv)
+LedStrip::LedStrip(uint32_t numPerStrip)
 {
-	stripLen = numPerStrip;
-	leds=ledsv;
+  stripLen = numPerStrip;
 }
 
 void LedStrip::begin(void)
 {
-	FastLED.addLeds<LED_TYPE, DATA_PIN1, COLOR_ORDER>(leds, 0, stripLen).setCorrection( TypicalLEDStrip );
-	FastLED.addLeds<LED_TYPE, DATA_PIN2, COLOR_ORDER>(leds, stripLen, stripLen * 2).setCorrection( TypicalLEDStrip );
-	FastLED.setBrightness(  BRIGHTNESS );
+  FastLED.addLeds<LED_TYPE, DATA_PIN1, COLOR_ORDER>(leds[0], stripLen).setCorrection( TypicalLEDStrip );
+  if (NUMBER_LEDSTRIP > 1)
+    FastLED.addLeds<LED_TYPE, DATA_PIN2, COLOR_ORDER>(leds[1], stripLen).setCorrection( TypicalLEDStrip );
+  if (NUMBER_LEDSTRIP > 2)
+    FastLED.addLeds<LED_TYPE, DATA_PIN3, COLOR_ORDER>(leds[2], stripLen).setCorrection( TypicalLEDStrip );
+
+  FastLED.setBrightness(  BRIGHTNESS );
+  FastLED.setDither( 0 );
 }
 
 void LedStrip::show(void)
 {
-	FastLED.show();
+  FastLED.show();
 }
 
 void LedStrip::setStripLength(uint16_t length)
 {
-	stripLen=length;
+  stripLen = length;
+  /*  FastLED[0].setLeds(leds[0], stripLen);
+    FastLED[1].setLeds(leds[1], stripLen);
+    FastLED[2].setLeds(leds[2], stripLen); */
 }
 
 
 void LedStrip::setPixel(uint32_t num, int color)
 {
-	/*uint32_t strip, offset, mask;
-	  uint8_t bit, *p;
-	   strip = num / stripLen;
-	   offset = num % stripLen;
-	   bit = (1<<strip);
-	   p = ((uint8_t *)drawBuffer) + offset * 24;
-	   for (mask = (1<<23); mask; mask >>= 1) {
-	       if (color & mask) {
-		 * p++ |= bit;
-		        } else {
-		 * p++ &= ~bit;
-		      }
-		  }
-		/leds[num]=color;
-	*/
-	leds[num].b=(color & 0xFF);  // Take just the lowest 8 bits.
-	leds[num].g=((color >> 8)& 0xFF);  // Shift the integer right 8 bits.
-	leds[num].r=((color >> 16)& 0xFF);  // Shift the integer right 8 bits.
+  uint32_t strip, offset;
+
+  strip = num / stripLen;
+  offset = num % stripLen;
+
+  leds[strip][offset].b = (color & 0xFF); // Take just the lowest 8 bits.
+  leds[strip][offset].g = ((color >> 8) & 0xFF); // Shift the integer right 8 bits.
+  leds[strip][offset].r = ((color >> 16) & 0xFF); // Shift the integer right 8 bits.
 }
 
 int LedStrip::getPixel(uint32_t num)
 {
-	/*
-	  uint32_t strip, offset, mask;
-	  uint8_t bit, *p;
-	  int color=0;
-	   strip = num / stripLen;
-	   offset = num % stripLen;
-	   bit = (1<<strip);
-	   p = ((uint8_t *)drawBuffer) + offset * 24;
-	   for (mask = (1<<23); mask; mask >>= 1) {
-	        if (*p++ & bit) color |= mask;
-	   }
-	  return color;
-	*/
-	return leds[num];
+  uint32_t strip, offset;
+
+  strip = num / stripLen;
+  offset = num % stripLen;
+  return leds[strip][offset];
 }
