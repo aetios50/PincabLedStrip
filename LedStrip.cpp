dedc154925dbf86129e84addd2f9df579a7e4cb4
aetios50 <frenchclanclan@gmail.com>
2018-06-01 12:04:22 +0200

    Update LedStrip.cpp

--- LedStrip.cpp
+++ LedStrip.cpp
@@ -20,7 +20,7 @@ void LedStrip::begin(void)
   if (NUMBER_LEDSTRIP > 2)
     FastLED.addLeds<LED_TYPE, DATA_PIN3, COLOR_ORDER>(leds[2], stripLen).setCorrection( TypicalLEDStrip );
   if (NUMBER_LEDSTRIP > 3)
-    FastLED.addLeds<LED_TYPE, DATA_PIN3, COLOR_ORDER>(leds[3], stripLen).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, DATA_PIN4, COLOR_ORDER>(leds[3], stripLen).setCorrection( TypicalLEDStrip );
   
   FastLED.setBrightness(  BRIGHTNESS );
   FastLED.setDither( 0 );
